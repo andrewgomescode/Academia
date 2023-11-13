e22cd282e635b1b0d2fbc89b7e3c284decfc9da4
andrewgomescode <andrewgomes.brc@gmail.com>
2023-11-13 06:30:11 -0300

    ver 1.0.4

---
+++ script.js
@@ -0,0 +1,19 @@
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+
+var $root = $('html, body');
+
+$('a[href^="#"]').click(function () {
+    $root.animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+
+    return false;
+});
